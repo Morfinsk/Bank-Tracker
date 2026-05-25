2626bac8cc850d6da397f799744d4212f0acafd1
Morfinsk <morfinnnn@azet.sk>
2026-05-25 13:28:09 +0200

    Update Firebase configuration and notification handling

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "",
+  apiKey: "AIzaSyDZtbXCeF1MTmsfnobMrj9KlOQTqYVmOOw",
   authDomain: "bank-card-tracker.firebaseapp.com",
   projectId: "bank-card-tracker",
   storageBucket: "bank-card-tracker.firebasestorage.app",
